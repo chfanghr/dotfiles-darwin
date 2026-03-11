003cfad8226dac7e997fb8cbdf07e56742b2917e
Hongrui Fang <chfanghr@gmail.com>
2026-03-12 00:29:33 +0800

    trust microvm cache

--- modules/nix-darwin/determinate.nix
+++ modules/nix-darwin/determinate.nix
@@ -3,6 +3,8 @@
     enable = true;
     customSettings = {
       eval-cores = 0;
+      trusted-substituters = ["https://microvm.cachix.org"];
+      trusted-public-keys = ["microvm.cachix.org-1:oXnBc6hRE3eX5rSYdRyMYXnfzcCxC7yKPTbZXALsqys="];
     };
     determinateNixd = {
       garbageCollector.strategy = "automatic";
