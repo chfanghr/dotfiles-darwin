3b70b13e1a0d0430f3feebc0d42a75beb5a087d8
Hongrui Fang <chfanghr@gmail.com>
2026-03-15 17:50:31 +0800

    add iog cache

--- modules/nix-darwin/determinate.nix
+++ modules/nix-darwin/determinate.nix
@@ -3,8 +3,8 @@
     enable = true;
     customSettings = {
       eval-cores = 0;
-      trusted-substituters = ["https://microvm.cachix.org"];
-      trusted-public-keys = ["microvm.cachix.org-1:oXnBc6hRE3eX5rSYdRyMYXnfzcCxC7yKPTbZXALsqys="];
+      substituters = ["https://microvm.cachix.org" "https://cache.iog.io"];
+      trusted-public-keys = ["microvm.cachix.org-1:oXnBc6hRE3eX5rSYdRyMYXnfzcCxC7yKPTbZXALsqys=" "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="];
     };
     determinateNixd = {
       garbageCollector.strategy = "automatic";
